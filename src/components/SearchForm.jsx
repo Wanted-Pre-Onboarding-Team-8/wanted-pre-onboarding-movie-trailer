23df783c3c762a4a2ecb403ebfc51f4c8e60858f
Kim-Jaemin420 <jany240@naver.com>
2022-09-07 05:22:12 +0900

    [#2] feat: 검색어 입력폼 기본 html 구조 작성

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SearchForm() {
+  return (
+    <form>
+      <legend>검색어 입력폼</legend>
+      <fieldset>
+        <label></label>
+        <input type="text" name="" id="" />
+        <input type="submit" value="" />
+      </fieldset>
+    </form>
+  );
+}
+
+export default SearchForm;
