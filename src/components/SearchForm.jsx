c62cce42045e01c288ff05aa843bc945a8ba267b
Kim-Jaemin420 <jany240@naver.com>
2022-09-07 23:30:37 +0900

    [#2] feat: search form 기본 ui 및 스타일 구현

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../style';
 
 export default function SearchForm() {
   return (
     <form>
-      <legend>검색어 입력폼</legend>
+      <legend className="a11yHidden">검색어 입력폼</legend>
       <fieldset>
-        <label></label>
-        <input type="text" name="" id="" />
-        <input type="submit" value="" />
+        <label htmlFor="keyword" className="a11yHidden">
+          검색어 입력
+        </label>
+        <SearchInput type="text" name="searchKeywordInput" id="keyword" />
+        <label htmlFor="search" className="a11yHidden">
+          검색
+        </label>
+        <SubmitInput type="submit" value="검색" id="search" />
       </fieldset>
     </form>
   );
 }
+
+const SearchInput = styled.input`
+  display: inline-block;
+  background-color: ${colors.white};
+  border: none;
+  width: 250px;
+  height: 22px;
+  line-height: 20px;
+`;
+
+const SubmitInput = styled.input`
+  border: none;
+  margin-left: -2px;
+  cursor: pointer;
+  width: 50px;
+  height: 24px;
+  line-height: 20px;
+  background-color: ${colors.white};
+`;
