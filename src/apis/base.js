f5650538a1ace18a872e3564b7a6b388b47d675e
baebumsoo <baebeomsu77@gmail.com>
2022-09-07 14:12:09 +0900

    [#1] modify: change-api-setting -axios base 설정 변경

--- src/apis/base.js
+++ src/apis/base.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
-import { BASE_URL } from '../consts/net';
+import { BASE_URL, HTTP_METHODS } from '../consts';
 
 const customAxios = axios.create({
   baseURL: BASE_URL,
-  method: 'get',
   params: { api_key: process.env.REACT_APP_API_KEY },
   headers: {
     'Content-Type': 'application/json',
   },
 });
+const handleRequest = (config) => {
+  return config;
+};
 
-export default customAxios;
+const createApiMethod = (axiosInstance, methodType) => (config) => {
+  return axiosInstance({ ...handleRequest(config), method: methodType });
+};
+
+export default {
+  get: createApiMethod(customAxios, HTTP_METHODS.GET),
+};
