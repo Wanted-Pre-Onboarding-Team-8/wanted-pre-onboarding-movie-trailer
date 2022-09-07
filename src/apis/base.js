3c07b1dc643f1f6eebcd3a6dd669993215371144
HyeonSeong <xkdtndua15@gmail.com>
2022-09-07 18:16:15 +0900

    [#23] modify: customAxios params에 인자 추가
    - customAxios params에 language: ko-KR 인자 추가

--- src/apis/base.js
+++ src/apis/base.js
@@ -3,7 +3,7 @@ import { BASE_URL, HTTP_METHODS } from '../consts';
 
 const customAxios = axios.create({
   baseURL: BASE_URL,
-  params: { api_key: process.env.REACT_APP_API_KEY },
+  params: { api_key: '88a9e902a5cfd8c9c19a18181208f960', language: 'ko-KR' },
   headers: {
     'Content-Type': 'application/json',
   },
