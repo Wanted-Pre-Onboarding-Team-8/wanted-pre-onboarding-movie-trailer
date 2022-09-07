29ad456e4e1880b738f17b9484c45f8779d0e310
Kim-Jaemin420 <jany240@naver.com>
2022-09-08 02:12:33 +0900

    [#2] feat: 검색했을때 결과 불러오는 api 로직 구현

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import { getMoviesBySearchKeyword } from '../apis';
 import { colors } from '../style';
+import { BsSearch as SearchIcon } from 'react-icons/bs';
 
 export default function SearchForm() {
   const handleSubmitSearchForm = async (event) => {
     event.preventDefault();
 
     const { searchKeywordInput } = event.currentTarget;
+    const keyword = searchKeywordInput.value;
+
+    try {
+      const { data } = await getMoviesBySearchKeyword(keyword);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmitSearchForm}>
+    <Wrapper onSubmit={handleSubmitSearchForm}>
       <legend className="a11yHidden">검색어 입력폼</legend>
       <fieldset>
         <label htmlFor="keyword" className="a11yHidden">
           검색어 입력
         </label>
         <SearchInput type="text" name="searchKeywordInput" id="keyword" />
-        <label htmlFor="search" className="a11yHidden">
-          검색
-        </label>
-        <SubmitInput type="submit" value="검색" id="search" />
+        <SubmitButton type="submit" aria-label="검색">
+          <SearchIcon />
+        </SubmitButton>
       </fieldset>
-    </form>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.form`
+  display: flex;
+  justify-content: center;
+  padding: 0;
+`;
 
 const SearchInput = styled.input`
   display: inline-block;
   background-color: ${colors.white};
   border: none;
-  width: 250px;
-  height: 22px;
+  width: 70%;
+  min-width: 230px;
+  height: 24px;
+  padding: 0 10px;
   line-height: 20px;
 `;
 
-const SubmitInput = styled.input`
+const SubmitButton = styled.button`
   border: none;
-  margin-left: -2px;
   cursor: pointer;
-  width: 50px;
   height: 24px;
   line-height: 20px;
   background-color: ${colors.white};
