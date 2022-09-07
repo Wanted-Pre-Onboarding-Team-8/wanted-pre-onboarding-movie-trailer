b18c57f0acdff2419dab241ab63c6144840894df
baebumsoo <baebeomsu77@gmail.com>
2022-09-07 13:24:10 +0900

    [#1] modify: change-page-directory-structure - 페이지 폴더 구조 변

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as DetailMovie } from './Movies/Detail/DetailMovie';
-export { default as NowPlayingMovies } from './Movies/NowPlaying/NowPlayingMovies';
-export { default as SearchMovies } from './Movies/Search/SearchMovies';
-export { default as TopRatedMovies } from './Movies/TopRated/TopRatedMovies';
-export { default as UpComingMovies } from './Movies/UpComing/UpComingMovies';
+export { default as DetailMovie } from './Detail/DetailMovie';
+export { default as NowPlayingMovies } from './NowPlaying/NowPlayingMovies';
+export { default as SearchMovies } from './Search/SearchMovies';
+export { default as TopRatedMovies } from './TopRated/TopRatedMovies';
+export { default as UpComingMovies } from './UpComing/UpComingMovies';
 export { default as HomePage } from './Home/HomePage';
